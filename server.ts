152fcc1dab595b78930afae756ae13ffe272b8cb
AryaTandon <atandon23@outlook.com>
2021-03-30 14:47:42 +0100

    Got vote functionality working!

--- server.ts
+++ server.ts
@@ -29,11 +29,12 @@ client.connect();
 app.get("/", async (req, res) => {
   const dbres = await client.query('SELECT res.id, res.title, res.author, res.url, ' +
   'res.description, rtg.cat_tags, rt.content_type, rec.recommender, rec.is_faculty, ' +
-  'rec.mark_stage, rec.was_used FROM resources res ' +
+  'rec.mark_stage, rec.was_used, rv.vote FROM resources res ' +
   'LEFT JOIN resource_type rt ON res.id = rt.id ' +
   'LEFT JOIN resource_tags rtg ON res.id = rtg.id ' +
   'LEFT JOIN recommendations rec ON res.id = rec.id ' +
-  'ORDER BY res.id DESC LIMIT 20;');
+  'LEFT JOIN resource_votes rv ON res.id = rv.id ' +
+  'ORDER BY rv.id DESC;');
   res.json(dbres.rows);
 });
 
@@ -41,29 +42,61 @@ app.get("/search/:search_term", async (req, res) => {
   const searchTerm = req.params.search_term
   const dbres = await client.query("SELECT res.id, res.title, res.author, res.url, " +
   "res.description, rtg.cat_tags, rt.content_type, rec.recommender, rec.is_faculty, " +
-  "rec.mark_stage, rec.was_used FROM resources res " +
+  "rec.mark_stage, rec.was_used, rv.vote FROM resources res " +
   "LEFT JOIN resource_type rt ON res.id = rt.id " +
   "LEFT JOIN resource_tags rtg ON res.id = rtg.id " +
   "LEFT JOIN recommendations rec ON res.id = rec.id " +
+  "LEFT JOIN resource_votes rv ON res.id = rv.id " +
   "WHERE res.title ILIKE '%'||$1||'%' OR " +
   "res.author ILIKE '%'||$1||'%' OR " +
   "res.description ILIKE '%'||$1||'%' OR " +
   "rtg.cat_tags ILIKE '%'||$1||'%';", [searchTerm]);
   res.json(dbres.rows);
-
 });
 
 app.get("/cat_tags/:search_term", async (req, res) => {
-  const searchTerm = req.params.search_term
+  const searchTerm = req.params.search_term;
   const dbres = await client.query("SELECT res.id, res.title, res.author, res.url, " +
   "res.description, rtg.cat_tags, rt.content_type, rec.recommender, rec.is_faculty, " +
-  "rec.mark_stage, rec.was_used FROM resources res " +
+  "rec.mark_stage, rec.was_used, rv.vote FROM resources res " +
   "LEFT JOIN resource_type rt ON res.id = rt.id " +
   "LEFT JOIN resource_tags rtg ON res.id = rtg.id " +
   "LEFT JOIN recommendations rec ON res.id = rec.id " +
+  "LEFT JOIN resource_votes rv ON res.id = rv.id " +
   "WHERE rtg.cat_tags ILIKE '%'||$1||'%';", [searchTerm]);
   res.json(dbres.rows);
+});
 
+app.post("/upvote/:id", async (req, res) => {
+  const resourceID = parseInt(req.params.id);
+  let dbres;
+  try {
+    let currentVoteCount: any = await client.query('SELECT vote FROM resource_votes ' +
+    'WHERE id = $1;', [resourceID]);
+    dbres = await client.query('UPDATE resource_votes SET vote = ($2 + 1) ' +
+    'WHERE id = $1 RETURNING *;', [resourceID, currentVoteCount.rows[0].vote]);
+  } catch (err) {
+    dbres = await client.query('INSERT INTO resource_votes (id, vote) ' +
+    'VALUES ($1, 1) RETURNING *;', [resourceID]);
+  } finally {
+    res.json(dbres.rows);
+  }
+});
+
+app.post("/downvote/:id", async (req, res) => {
+  const resourceID = parseInt(req.params.id);
+  let dbres;
+  try {
+    let currentVoteCount: any = await client.query('SELECT vote FROM resource_votes ' +
+    'WHERE id = $1;', [resourceID]);
+    dbres = await client.query('UPDATE resource_votes SET vote = ($2 - 1) ' +
+    'WHERE id = $1 RETURNING *;', [resourceID, currentVoteCount.rows[0].vote]);
+  } catch (err) {
+    dbres = await client.query('INSERT INTO resource_votes (id, vote) ' +
+    'VALUES ($1, -1) RETURNING *;', [resourceID]);
+  } finally {
+    res.json(dbres.rows);
+  }
 });
 
 app.post("/", async (req, res) => {
