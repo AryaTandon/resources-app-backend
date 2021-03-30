14f24ce9ec4e9c248eab8afef5847528e8f5a0aa
Olivia Neil-Jones <olivianeiljones@gmail.com>
2021-03-30 12:02:56 +0200

    GET searchbar completed

--- server.ts
+++ server.ts
@@ -28,13 +28,29 @@ client.connect();
 
 app.get("/", async (req, res) => {
   const dbres = await client.query('SELECT res.id, res.title, res.author, res.url, ' +
-  'res.description, rt.cat_tags, rt.content_type, rec.recommender, rec.is_faculty, ' +
+  'res.description, rtg.cat_tags, rt.content_type, rec.recommender, rec.is_faculty, ' +
   'rec.mark_stage, rec.was_used FROM resources res ' +
   'LEFT JOIN resource_type rt ON res.id = rt.id ' +
   'LEFT JOIN resource_tags rtg ON res.id = rtg.id ' +
   'LEFT JOIN recommendations rec ON res.id = rec.id ' +
   'ORDER BY res.id DESC LIMIT 20;');
   res.json(dbres.rows);
+});
+
+app.get("/:search_term", async (req, res) => {
+  const searchTerm = req.params.search_term
+  const dbres = await client.query("SELECT res.id, res.title, res.author, res.url, " +
+  "res.description, rtg.cat_tags, rt.content_type, rec.recommender, rec.is_faculty, " +
+  "rec.mark_stage, rec.was_used FROM resources res " +
+  "LEFT JOIN resource_type rt ON res.id = rt.id " +
+  "LEFT JOIN resource_tags rtg ON res.id = rtg.id " +
+  "LEFT JOIN recommendations rec ON res.id = rec.id " +
+  "WHERE res.title ILIKE '%'||$1||'%' OR " +
+  "res.author ILIKE '%'||$1||'%' OR " +
+  "res.description ILIKE '%'||$1||'%' OR " +
+  "rtg.cat_tags ILIKE '%'||$1||'%';", [searchTerm]);
+  res.json(dbres.rows);
+
 });
 
 app.post("/", async (req, res) => {
